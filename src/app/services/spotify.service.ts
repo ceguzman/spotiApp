b72e001d1aa7217249c1b38b44d11e520f2cb9c8
Carlos <ceguzman582@misena.edu.co>
2020-09-25 23:10:26 -0500

    chore: creando el metodo getArtistas

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -6,13 +6,20 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 })
 export class SpotifyService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   getNewRelease(): any {
     const headers = new HttpHeaders({
-      Authorization: 'Bearer BQDZRuAL-P_JK0Me_zvQXG00VWklxNFTT8aX_vdYx8FtUB-FY5oD6MjNHS9Qgume7fNxHApm93JIAvwkC00'
+      Authorization: 'Bearer BQDL8gXJE0MpMH0MnlZJr1eDC8ElUdQl8PoHn-DIOHnrPzJHHEuR20hwRoX45KWwaxRPUdEBLzy2iXSsSlc'
     });
-
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers});
+  }
+
+  getArtistas(termino: string): any {
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer BQDL8gXJE0MpMH0MnlZJr1eDC8ElUdQl8PoHn-DIOHnrPzJHHEuR20hwRoX45KWwaxRPUdEBLzy2iXSsSlc'
+    });
+    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers});
   }
 }
