61b3d07502ae305da4b6c6e211d3bf9c58b79081
Carlos <ceguzman582@misena.edu.co>
2020-09-25 23:39:53 -0500

    chore: creando un metodo para organizar el header y las consultas

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -11,19 +11,23 @@ export class SpotifyService {
   constructor(private http: HttpClient) {
   }
 
-  getNewRelease(): any {
+  getQuery(query: string): any {
+    const url = `https://api.spotify.com/v1/${query}`;
+
     const headers = new HttpHeaders({
       Authorization: 'Bearer BQDL8gXJE0MpMH0MnlZJr1eDC8ElUdQl8PoHn-DIOHnrPzJHHEuR20hwRoX45KWwaxRPUdEBLzy2iXSsSlc'
     });
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers})
+
+    return this.http.get(url, {headers});
+  }
+
+  getNewRelease(): any {
+    return this.getQuery('browse/new-releases')
       .pipe(map((data: any) => data.albums.items));
   }
 
   getArtistas(termino: string): any {
-    const headers = new HttpHeaders({
-      Authorization: 'Bearer BQDL8gXJE0MpMH0MnlZJr1eDC8ElUdQl8PoHn-DIOHnrPzJHHEuR20hwRoX45KWwaxRPUdEBLzy2iXSsSlc'
-    });
-    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers})
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
       .pipe(map((data: any) => data.artists.items));
   }
 }
