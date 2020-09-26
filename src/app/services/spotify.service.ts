b3c6e7872e7ee70aade127f5a0206ba2b8d69b57
Carlos <ceguzman582@misena.edu.co>
2020-09-25 23:32:49 -0500

    chore: usando la propiedad map para filtrar los datos

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,13 +15,15 @@ export class SpotifyService {
     const headers = new HttpHeaders({
       Authorization: 'Bearer BQDL8gXJE0MpMH0MnlZJr1eDC8ElUdQl8PoHn-DIOHnrPzJHHEuR20hwRoX45KWwaxRPUdEBLzy2iXSsSlc'
     });
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers});
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers})
+      .pipe(map((data: any) => data.albums.items));
   }
 
   getArtistas(termino: string): any {
     const headers = new HttpHeaders({
       Authorization: 'Bearer BQDL8gXJE0MpMH0MnlZJr1eDC8ElUdQl8PoHn-DIOHnrPzJHHEuR20hwRoX45KWwaxRPUdEBLzy2iXSsSlc'
     });
-    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers});
+    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers})
+      .pipe(map((data: any) => data.artists.items));
   }
 }
