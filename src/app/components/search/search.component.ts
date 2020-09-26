9f6a496d6465ac7f5cba36f4d63e1eba716c47dc
Carlos <ceguzman582@misena.edu.co>
2020-09-25 23:10:08 -0500

    chore: agregando el metodo buscar en el search component

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SpotifyService} from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  artistas: any[] = [];
+
+  constructor(private spotifyService: SpotifyService) {
+  }
 
   ngOnInit(): void {
+  }
+
+  buscar(termino: string): void {
+    console.log(termino);
+    this.spotifyService.getArtistas(termino)
+      .subscribe((data: any) => {
+        console.log(data.artists.items);
+        this.artistas = data.artists.items;
+      });
   }
 
 }
