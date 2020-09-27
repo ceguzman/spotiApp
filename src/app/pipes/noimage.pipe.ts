4c638acf7c5a4f0b166b720ba3d525944a058dd3
Carlos <ceguzman582@misena.edu.co>
2020-09-27 17:36:53 -0500

    chore: creando un pipe,  para validar si no hay una imagen existente

--- src/app/pipes/noimage.pipe.ts
+++ src/app/pipes/noimage.pipe.ts
@@ -1,12 +1,21 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
   name: 'noimage'
 })
 export class NoimagePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(image: any[]): string {
+    if (!image) {
+      return 'assets/img/no-image.png';
+    }
+    if (image.length > 0) {
+      return image[0].url;
+    } else {
+      return 'assets/img/no-image.png';
+    }
+
+
   }
 
 }
