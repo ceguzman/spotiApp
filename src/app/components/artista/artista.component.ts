bad9685492d7a4d42d25a85878494d4036494224
Carlos <ceguzman582@misena.edu.co>
2020-09-28 14:15:48 -0500

    chore: agregando la logica en artista component

--- src/app/components/artista/artista.component.ts
+++ src/app/components/artista/artista.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {SpotifyService} from '../../services/spotify.service';
 
 @Component({
   selector: 'app-artista',
@@ -8,13 +9,27 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class ArtistaComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  artista: any = {};
+  loading: boolean;
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private spotifyService: SpotifyService) {
+    this.loading = true;
     activatedRoute.params.subscribe(params => {
-      console.log(params.id);
+      this.getArtista(params.id);
     });
   }
 
   ngOnInit(): void {
+  }
+
+  getArtista(id: string): any {
+    this.loading = true;
+    this.spotifyService.getArtista(id)
+      .subscribe(artista => {
+        this.artista = artista;
+        this.loading = false;
+      });
   }
 
 }
