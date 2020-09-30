6e56aba99090c4b36525f98df4f89c3a3393cf9e
Carlos <ceguzman582@misena.edu.co>
2020-09-30 18:42:24 -0500

    chore: alerta component

---
+++ src/app/components/shared/alerta/alerta.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alerta',
+  templateUrl: './alerta.component.html',
+})
+export class AlertaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
