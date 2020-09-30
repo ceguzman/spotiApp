734fc38aa7dafd72b3061f054663ad120297e110
Carlos <ceguzman582@misena.edu.co>
2020-09-30 18:59:37 -0500

    chore: input en el alerta component

--- src/app/components/shared/alerta/alerta.component.ts
+++ src/app/components/shared/alerta/alerta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-alerta',
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertaComponent implements OnInit {
 
-  constructor() { }
+  @Input() mensajeEror: string;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
